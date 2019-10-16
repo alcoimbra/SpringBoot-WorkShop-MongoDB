ad3e17dd05862c79f31cb5a10407853c2581ab9c
Alan Gomes Coimbra <algcoimbra01@gmail.com>
2019-10-16 14:45:55 -0300

    Insercao de Usuario com POST

--- src/main/java/br/com/capgemini/workshop/resource/UserResource.java
+++ src/main/java/br/com/capgemini/workshop/resource/UserResource.java
@@ -1,14 +1,17 @@
 package br.com.capgemini.workshop.resource;
 
+import java.net.URI;
 import java.util.List;
 import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import br.com.capgemini.workshop.domain.User;
 import br.com.capgemini.workshop.dto.UserDTO;
@@ -34,5 +37,15 @@ public class UserResource {
 		User user = userService.findById(id);
 		
 		return ResponseEntity.ok().body(new UserDTO(user));
+	}
+	
+	@RequestMapping(method=RequestMethod.POST)
+	public ResponseEntity<Void> insert(@RequestBody UserDTO userDTO){
+		User user = userService.fromDTO(userDTO);
+		user = userService.insert(user);
+		
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(user.getId()).toUri();
+		
+		return ResponseEntity.created(uri).build();
 	}
 }
--- src/main/java/br/com/capgemini/workshop/service/UserService.java
+++ src/main/java/br/com/capgemini/workshop/service/UserService.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import br.com.capgemini.workshop.domain.User;
+import br.com.capgemini.workshop.dto.UserDTO;
 import br.com.capgemini.workshop.repository.UserRepository;
 import br.com.capgemini.workshop.service.exception.ObjectNotFoundException;
 
@@ -24,5 +25,13 @@ public class UserService {
 		Optional<User> user = userRepository.findById(id);
 		
 		return user.orElseThrow(() -> new ObjectNotFoundException("Objeto Não Encontrado"));
+	}
+	
+	public User insert(User user) {
+		return userRepository.insert(user);
+	}
+	
+	public User fromDTO(UserDTO userDTO) {
+		return new User(userDTO.getId(), userDTO.getName(), userDTO.getEmail());
 	}
 }
